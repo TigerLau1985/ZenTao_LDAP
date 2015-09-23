cab9ad3432737dc85b2ebe5380d0a5245609c8f6
TigerLau1985 <tigerliu1985@hotmail.com>
2015-09-23 17:33:23 +0800

    上传所有文件
    实现了类似ZenTao专业版的LDAP功能

---
+++ ldap/module/ldap/config.php
@@ -0,0 +1,11 @@
+<?php 
+$config->ldap = new stdclass();
+$config->ldap->host = '';
+$config->ldap->version = '';
+$config->ldap->bindDN = '';
+$config->ldap->bindPWD = '';
+$config->ldap->baseDN = '';
+$config->ldap->searchFilter = '';
+$config->ldap->uid = '';
+$config->ldap->mail = '';
+$config->ldap->name = '';
